73720e5c7b352145211fc3a383083f25aca50b1e
osadezu <osadezu@gmail.com>
2021-12-08 14:29:21 -0500

    Develop functionality to brighten, darken and pulse gems

--- script.js
+++ script.js
@@ -14,28 +14,67 @@ const GEM_COLORS = [
   '#FF5722',
 ];
 
+const STEP_DURATION = 350;
+
+// Global variables
+let = sequenceSteps = [];
+
 // HTML element selectors
 const gems = document.querySelectorAll('.gem');
 const board = document.querySelector('#board');
 
-// Buttons event listener
-board.addEventListener('click', (event) => {
+// Event listeners
+board.addEventListener('mousedown', (event) => {
   if (event.target.classList.contains('gem')) {
-    event.target.classList.toggle('bright');
+    brightenGem(event.target);
+  }
+});
+board.addEventListener('mouseup', (event) => {
+  if (event.target.classList.contains('gem')) {
+    darkenGem(event.target);
+  }
+});
+board.addEventListener('mouseout', (event) => {
+  if (event.target.classList.contains('gem')) {
+    darkenGem(event.target);
   }
 });
 
-// Test colors
-gems.forEach((element, i) => {
-  element.style.backgroundColor = GEM_COLORS[i];
-  element.dataset.gemId = i;
-});
+/********************
+ *  View Functions  *
+ ********************/
 
-// Functions
+function brightenGem(gem) {
+  gem.classList.add('bright');
+}
+
+function darkenGem(gem) {
+  gem.classList.remove('bright');
+}
+
+function pulseGem(gem) {
+  brightenGem(gem);
+  console.log('Gem on');
+  setTimeout(darkenGem, STEP_DURATION, gem);
+  console.log('Gem off requested');
+}
+
+// TODO: dynamic creation of buttons
+
+/********************
+ *  Game Functions  *
+ ********************/
+
+function addRandomStep() {
+  sequenceSteps.push(Math.floor(Math.random() * 16));
+}
+
 function playSequence() {
-  // LOOP for all elements in sequence
+  // LOOP through all elements in sequence
+  sequenceSteps.forEach();
   // Brighten button
-  // Play tone (?)
+
+  // TODO: Play tone
   // long Delay
   // Darken button
   // short delay
@@ -48,3 +87,16 @@ function listenForSequence() {
   // if incorrect > lose round
   // if correct, advance step in sequence and listen for next step
 }
+
+// TODO: Below code is for tests, bake into final functionality
+
+// Test colors
+gems.forEach((element, i) => {
+  element.style.backgroundColor = GEM_COLORS[i];
+  element.id = i;
+});
+
+for (let i = 0; i < 5; i++) {
+  addRandomStep();
+}
+console.log(sequenceSteps);
